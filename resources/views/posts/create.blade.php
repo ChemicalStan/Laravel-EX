75b8c2e4223005a7139ba017a58f85d874cec252
fototell <chemicalstan15@gmail.com>
2018-11-28 16:25:59 +0100

    validation with laravel

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -3,12 +3,21 @@
 @section('content')
 <h1>Create Post</h1>
 {{-- <form method="post" action="{{ action('PostsController@store')}}" enctype="multipart/form-data"> --}}
-{!! form::open() !!}
-    {{ csrf_field() }}
-        <input type="text" placeholder="Enter Title" name="title">
+{!! form::open(['method'=>'POST', 'action'=>'PostsController@store']) !!}
+    {{-- {{ csrf_field() }} --}}
+    <div class="form-group">
+    {!!form::label('title', 'Title:')!!}
+    {!!form::text('title', null, ['class'=>'form-control'])!!}
+    {!!form::label('content', 'Content:')!!}
+    {!!form::text('content', null, ['class'=>'form-control'])!!}
+    </div>
+    <div class="form-group">
+        {!!form::submit('Create Post', ['class'=>'btn btn-primary'])!!}
+    </div>
+        {{-- <input type="text" placeholder="Enter Title" name="title">
         <input type="text" placeholder="Enter Content" name="content">
-        <input type="submit" name="submit">
-  </form>
+        <input type="submit" name="submit"> --}}
+{!! form::close()!!}
 @stop
 @section('footer')
 
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,18 +2,20 @@
 
 @section('content')
 <h1>Edit Post</h1>
-<form method="post" action="{{ action('PostsController@update', $post->id)}}">
-    {{ csrf_field() }}
-    <input type="hidden" name="_method" value="PUT">
-    <input type="text" name="title" value="{{$post->title}}">
-    <input type="text" name="content" value="{{$post->content}}">
-    <input type="submit" name="submit">
-</form>
-<form method="POST" action="{{action('PostsController@destroy', $post->id)}}">
-    @csrf
-    <input type="hidden" name="_method" value="DELETE">
-    <input type="submit" name="delete" value="Delete">
-</form>
+{{-- <form method="post" action="{{ action('PostsController@update', $post->id)}}"> --}}
+{!!Form::model($post, ['method'=>'PATCH', 'action'=>['PostsController@update', $post->id]])!!}
+
+    {!!Form::label('title', 'Title:')!!}
+    {!!Form::text('title', $post->title, ['class'=>'form-control'])!!}
+    {!!Form::label('content', 'Content:')!!}
+    {!!Form::text('content', $post->content, ['class'=>'form-control'])!!}
+    {!!Form::submit('Update Post', ['class'=>'btn btn-primary'])!!}
+
+{!!Form::close()!!}
+{{-- <form method="POST" action="{{action('PostsController@destroy', $post->id)}}"> --}}
+{!!Form::open(['method'=>'DELETE', 'action'=>['PostsController@destroy', $post->id]])!!}
+    {!!Form::submit('Delete', ['class'=>'btn btn-danger'])!!}
+{!!Form::close()!!}
 @stop
 @section('footer')
 
